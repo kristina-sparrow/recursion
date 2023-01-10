ef9246eb00f0ddb2d05f7f7fa9606cbdf5e5a9b9
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-01-10 13:39:54 -0700

    Added fibonacci (exercise 1)

---
+++ fib.js
@@ -0,0 +1,23 @@
+// non-recursive
+function fibs(n) {
+  if (n <= 0) return "Please enter a number greater than 0";
+  const arr = [0, 1];
+  if (n < 2) return arr.slice(0, n);
+  while (n > 2) {
+    const newNum = arr[arr.length - 1] + arr[arr.length - 2];
+    arr.push(newNum);
+    n--;
+  }
+  return arr;
+}
+
+//recursive
+function fibsRec(n) {
+  return n <= 0
+    ? "Please enter a number greater than 0"
+    : n === 1
+    ? [0]
+    : n === 2
+    ? [0, 1]
+    : [...fibsRec(n - 1), fibsRec(n - 1)[n - 2] + fibsRec(n - 1)[n - 3]];
+}
