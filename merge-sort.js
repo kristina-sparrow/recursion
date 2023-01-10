441c271b6d56ce726ac098345228e283b733dabf
kristina-sparrow <118679841+kristina-sparrow@users.noreply.github.com>
2023-01-10 14:50:02 -0700

    Added merge sort (exercise 2)

--- merge-sort.js
+++ merge-sort.js
@@ -1,3 +1,18 @@
 function mergeSort(arr) {
-  return;
+  if (arr.length === 0) return "Please provide a non empty array";
+  if (arr.length < 2) return arr;
+  const middle = Math.floor(arr.length / 2);
+  const left = arr.slice(0, middle);
+  const right = arr.slice(middle);
+  return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+  const result = [];
+  let i = 0;
+  let j = 0;
+  while (i < left.length && j < right.length) {
+    left[i] < right[j] ? result.push(left[i++]) : result.push(right[j++]);
+  }
+  return result.concat(left.slice(i)).concat(right.slice(j));
 }
